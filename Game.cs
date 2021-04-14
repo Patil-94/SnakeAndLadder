a1bb2f4118bb1caa18015f6d6b35a8057a38a3a2
Patil-94 <ankitakhairnar10@.com>
2021-04-14 18:22:34 +0530

    [Ankita] Add:Two Player Game Implemented

--- Game.cs
+++ Game.cs
@@ -15,38 +15,48 @@ namespace SnakeAndLadder
         const int WINNING_POSITION = 100;
         public void Play()  
         {
-            int player = 0, dieCount = 0;
-            while (player < WINNING_POSITION)
+            int[] player = new int[2] { 0, 0 };
+            int dieCount = 0;
+            int chance = 0;
+            while (player[chance] < WINNING_POSITION)
             {
                 Random random = new Random();
                 int option = random.Next(0, 3);
                 int dieValue = random.Next(1, 7);
                 dieCount++;
-                Console.WriteLine("die count = " + dieCount);
+                
                 switch (option)
                 {
                     case NO_PLAY:
                         Console.WriteLine("No Play, Pass the chance");
                         break;
                     case LADDER:
-                        player += dieValue;
-                        if (player > WINNING_POSITION)
+                        if (player[chance] + dieValue <= WINNING_POSITION)
                         {
-                            Console.WriteLine("Invalid position");
-                            player -= dieValue;
+                            player[chance] += dieValue;
                         }
-                        Console.WriteLine("You got Ladder \n player position = " + player);
+                        Console.WriteLine("You got Ladder \n player = " + chance + "->" + " position " + "  " + player[chance]);
                         break;
                     case SNAKE:
-                        player -= dieValue;
-                        if (player < START_POSITION)
+                        if (player[chance] - dieValue < START_POSITION)
                         {
-                            player = START_POSITION;
+                            player[chance] = START_POSITION;
                         }
-                        Console.WriteLine("You gotSnake \n player position = " + player);
+                        else
+                        {
+                            player[chance] -= dieValue;
+                        }
+                        Console.WriteLine("You gotSnake \n player = " + chance + "-> position " + " " + player[chance]);
                         break;
                     default:
                         break;
+                }
+                if (option == NO_PLAY || option == SNAKE)
+                {
+                    if (chance == 0)
+                        chance = 1;
+                    else
+                        chance = 0;
                 }
             }
             Console.WriteLine("Total number of times die was rolled : " + dieCount);
